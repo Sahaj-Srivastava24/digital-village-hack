de4bdf5e79febfea0a80c676daea50e392c25c0e
Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
2022-01-15 17:02:37 +0530

    guidelines set

--- src/components/data.js
+++ src/components/data.js
@@ -6,27 +6,30 @@ const problemStatements = [
     },
     {
         id: 1,
-        title: 'Problem Statement 1',
+        title: 'Problem Statement 2',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.'
     },
     {
         id: 1,
-        title: 'Problem Statement 1',
+        title: 'Problem Statement 3',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.'
     },
     {
         id: 1,
-        title: 'Problem Statement 1',
+        title: 'Problem Statement 4',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.'
     }
 ]
 
 const guidelines = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+    "The hackathon is going to be conducted online and is open to students of all colleges/universities across India and Tel Aviv University.",
+    "Each team needs to register on the platform by <date>.",
+    "Duration of the hackathon is <start date> to <end date>.",
+    "Team size- ",
+    "Submission date of solutions- ",
+    "Each team needs to select at least one problem statement.",
+    "There is no registration fee.",
+    "Winning teams of each problem statement category will be awarded a prize money of Rs. 10,000."
 ]
 
 module.exports = {
